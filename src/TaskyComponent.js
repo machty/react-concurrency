dd54d311462c0edb4c697e63be0998cec5425941
machty <machty@gmail.com>
2019-06-21 08:16:45 -0400

    demonstrate taskInstance derived state

--- src/TaskyComponent.js
+++ src/TaskyComponent.js
@@ -14,19 +14,45 @@ class TaskyComponent extends Component {
           this.setState({ count });
           yield timeout(20);
         }
+        return Math.floor(Math.random() * 100);
       }
-    })
+    }),
+
+    defaultTask: task(this, {
+      *perform() {
+        // by default, tasks are `.drop()` and they are stateless.
+        // In order to access .isRunning/etc, you need to do `trackState: true`,
+        // (if you don't, it throws an error when you access that property)
+        yield timeout(500);
+        return Math.floor(Math.random() * 100);
+      }
+    }),
+
   };
 
   render() {
+    let lastSuccessful = this.state.myTask.lastSuccessful;
+
     return (
       <div>
-        <p>Count is {this.state.count}</p>
+        <p>
+          Count is {this.state.count}
+        </p>
+        {
+          lastSuccessful &&
+          <p>
+            The last value returned was {lastSuccessful.value}
+          </p>
+        }
         <p>
           <button onClick={() => this.state.myTask.perform(50)}>
             {this.state.myTask.isRunning
               ? "Performing..."
               : "Perform from Parent"}
+          </button>
+          <br/>
+          <button onClick={() => this.state.myTask.cancelAll()}>
+            myTask.cancelAll()
           </button>
         </p>
         <TaskyChildComponent task={this.state.myTask} />
